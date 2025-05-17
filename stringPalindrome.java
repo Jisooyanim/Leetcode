636541087c1cec07213d8455bf399f9b89a476b2
Jisooyanim <vladzzzzzss@gmail.com>
2025-05-17 16:21:06 +0800

    String palindrome

---
+++ stringPalindrome.java
@@ -0,0 +1,30 @@
+public class stringPalindrome {
+    public static void main(String[] args) {
+        String s = " ";
+        System.out.println(Solution(s));
+    }
+
+    public static boolean Solution(String s) {
+        StringBuilder cleaned = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        s = cleaned.toString();
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
