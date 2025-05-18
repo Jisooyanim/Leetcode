c0f5b63cc878d5b14ba0b05d26eb3e6582518222
Jisooyanim <vladzzzzzss@gmail.com>
2025-05-18 15:35:42 +0800

    Add digits notes

--- addDigits.java
+++ addDigits.java
@@ -5,6 +5,10 @@ public class addDigits {
     }
 
     public static int Solution(int num) {
-        return 1 + (num -1) % 9;
+        return 1 + (num - 1) % 9;
     }
 }
+// Digital root
+// The digital root of a number is equal to the remainder when 
+// that number is divided by 9.
+//https://brilliant.org/wiki/digital-root/#:~:text=The%20digital%20root%20or%20digital,single%2Ddigit%20number%20is%20reached.
